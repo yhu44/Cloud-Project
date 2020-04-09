a6ba2ea94b2f732743f6e52c8120cb95253d28ca
Julian White <jawhite1612@gmail.com>
2020-04-09 01:31:06 +0000

    updated packet tracers

--- startFatTree.py
+++ startFatTree.py
@@ -1,7 +1,23 @@
 import os
+from mininet.net import Mininet
+from mininet.link import TCLink
+from mininet.cli import CLI
+from mininet.clean import cleanup
 import time
+from fattree  import FatTreeTopology
 
+print("Cleaning up..")
+cleanup()
+topo = FatTreeTopology()
+net = Mininet(topo, link=TCLink)
+print("Building topology...")
+net.start()
+
+print("Running spanning tree protocol...")
 for i in range(1, 21):
     os.system('sudo ovs-vsctl set Bridge "%s" stp_enable=true' % ('s'+ str(i)))
+
 time.sleep(20*2)
 print("Done!")
+
+CLI(net)
--- traceAllPackets.py
+++ traceAllPackets.py
@@ -1,11 +1,13 @@
 import os
 
+allPaths = set()
+
 for i in range (1,21):
     os.system('sudo ovs-ofctl dump-flows {} > flows_{}.txt'.format('s' + str(i), 's' + str(i)))
 
 for sr in range(1, 17):
     for ds in range(sr+1, 17):
-        switches = []
+        switches = ''
         src = '10.0.0.' + str(sr)
         dst = '10.0.0.' + str(ds)
         for i in range(1,21):
@@ -14,6 +16,10 @@ for sr in range(1, 17):
             entries = f.readlines()
             for s in entries:
                 if (s.find('nw_src={},'.format(src))!= -1 and s.find('nw_dst={},'.format(dst)) != -1 and s.find('icmp_type=0,') != -1):
-                    switches.append(switch)
+                    switches += " " + switch
         print(src + " --> " + dst)
         print(switches)
+        allPaths.add(switches)
+
+print("Totoal number of unique paths: " + str(len(allPaths)))
+os.system('rm flows*')
